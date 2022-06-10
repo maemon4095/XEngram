f2e2fc50d2a7bfcf981d1aaa0f1ae59f37180386
maemon <47819358+maemon4095@users.noreply.github.com>
2022-06-10 14:30:52 +0900

    rename

--- src/XEngram/Program.cs
+++ src/XEngram/Program.cs
@@ -1,10 +1,11 @@
-using AlterNote;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using XEngram;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
